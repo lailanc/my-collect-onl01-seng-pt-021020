6696f9174c87e101d9f05513250406968819dd3b
Laila Campbell <lailacampbell96@gmail.com>
2020-02-21 15:59:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect (names)
 empty_array = []
-    counter = 0
-      my_collect(empty_array) do |x|
-        counter += 1
-      end
+counter = 0
+my_collect(empty_array) do |x|
+counter += 1
+end
 
